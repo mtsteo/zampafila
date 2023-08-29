d4e93306f9113ea2fb09cb8bb8f9a00ec56571fb
mtsteo <mateusteotonho@gmail.com>
2023-08-29 07:23:35 -0300

    firebase configured

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Será que vai da certo?</Text>
+      <Text>{process.env.APP_APYKEY}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: process.env.APP_APIKEY,
+    authDomain: process.env.APP_AUTHDOMAIN,
+    projectId: process.env.APP_PROJECTID,
+    storageBucket: process.env.APP_STORAGEBUCKET,
+    messagingSenderId: process.env.APP_MESSASINGSPENDERID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.APP_MEASUREMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export  default getFirestore(app)
