4753c2795a612a459ffd9e448aa2ee6a1f022491
mtsteo <mateusteotonho@gmail.com>
2023-09-02 19:46:00 -0300

    FEAT: buttons tabs stylized

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Header from "./components/Header";
 import EmEspera from "./screens/emEspera/EmEspera";
 import Atendidos from "./screens/atendidos/Atendidos";
-
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./styles/colors";
 export default function App() {
- 
   const Tab = createBottomTabNavigator();
 
   return (
@@ -14,9 +14,36 @@ export default function App() {
       <Header />
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown:false}}>
-          <Tab.Screen name="Em espera" component={EmEspera} />
-          <Tab.Screen name="Atendidos" component={Atendidos} />
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: { height: "12%" },
+            tabBarActiveBackgroundColor: colors.mainColors.primary,
+            tabBarActiveTintColor: colors.mainColors.textColors.textPrimary,
+            tabBarLabelStyle: { fontWeight: 700, fontSize: 20 },
+          }}
+        >
+          <Tab.Screen
+            name="Em espera"
+            component={EmEspera}
+            options={{
+              tabBarInactiveTintColor: colors.mainColors.secundary,
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="clockcircleo" size={28} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Atendidos"
+            component={Atendidos}
+            options={{
+              tabBarInactiveTintColor: colors.mainColors.primary,
+              tabBarActiveBackgroundColor: colors.mainColors.secundary,
+              tabBarIcon: ({ color, size }) => (
+                <AntDesign name="checkcircleo" size={28} color={color} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </>
--- styles/colors.js
+++ styles/colors.js
@@ -1,6 +1,7 @@
 export default {
     mainColors: {
       primary: "#7BDC6A",
+      secundary:"#f0bf2e",
       textColors:{
         textPrimary:"white",
       }
